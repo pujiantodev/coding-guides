fdd45a5da6ecf1c74d933f2d5137f7f94de01f8f
Eko <ekoxyz15@gmail.com>
2025-06-09 09:34:40 +0700

    docs: update title and tagline in configuration and index files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Laravelku",
+      title: "Pujianto's Code",
       social: [
         {
           icon: "github",
